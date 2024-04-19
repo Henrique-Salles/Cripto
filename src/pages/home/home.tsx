4f76de3891d286c95025764f96b2f9e496b52782
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-19 14:55:20 -0300

    search

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,30 @@
+import { useState, FormEvent } from "react";
 import styles from "./home.module.css";
 import { BsSearch } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function Home() {
+  const [input, setInput] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    navigate(`/details/${input}`);
+  }
+
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
-        <input type="text" placeholder="Digite o nome da moeda..." />
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o nome da moeda..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button type="submit">
           <BsSearch size={30} color="#fff" />
         </button>
