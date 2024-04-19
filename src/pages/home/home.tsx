7bf8145cdb20776373d5fd74069c0d74d1c64886
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-19 13:43:59 -0300

    home page form

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,15 @@
 import styles from "./home.module.css";
+import { BsSearch } from "react-icons/bs";
 
 export function Home() {
   return (
-    <div>
-      <h1>Welcome to Home Page</h1>
-    </div>
+    <main className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" placeholder="Digite o nome da moeda..." />
+        <button type="submit">
+          <BsSearch size={30} color="#fff" />
+        </button>
+      </form>
+    </main>
   );
 }
