e87518c8c1fefc1a493c2e298d7a782d29f7050b
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-19 13:56:52 -0300

    table

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,6 @@
 import styles from "./home.module.css";
 import { BsSearch } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 export function Home() {
   return (
@@ -10,6 +11,46 @@ export function Home() {
           <BsSearch size={30} color="#fff" />
         </button>
       </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Moeda</th>
+            <th scope="col">Valor de mercado</th>
+            <th scope="col">Preço</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Mudança 24h</th>
+          </tr>
+        </thead>
+
+        <tbody id="tbody">
+          <tr className={styles.tr}>
+            <td className={styles.tdLabel} data-label="Moeda">
+              <div className={styles.name}>
+                <Link to={"/details/bitcoin"}>
+                  <span>Bitcoin</span> | BTC
+                </Link>
+              </div>
+            </td>
+
+            <td className={styles.tdLabel} data-label="Valor de mercado">
+              1T
+            </td>
+
+            <td className={styles.tdLabel} data-label="Preço">
+              8.000
+            </td>
+
+            <td className={styles.tdLabel} data-label="Volume">
+              2B
+            </td>
+
+            <td className={styles.tdLabel} data-label="Mudança 24h">
+              <span>1.20</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
